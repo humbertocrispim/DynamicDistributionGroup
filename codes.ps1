3cff6e5e2497dcae70e5f5a7837ac5fe65e21c7a
Humberto Cunha Crispim <79642492+humbertocrispim@users.noreply.github.com>
2023-03-25 16:11:10 -0300

    Delete codes.ps1
    teste

--- codes.ps1
+++
@@ -1,8 +0,0 @@
-# Criando um grupo de distribuição dinâmico para líderes da GAV Resorts
-New-DynamicDistributionGroup -Name "GRUPO - Lideres GAV Resorts" -RecipientFilter {(RecipientType -eq 'UserMailbox') -and (Title -like 'LIDER*')}
-
-# Criando um grupo de distribuição dinâmico para todos os usuários da GAV Resorts
-New-DynamicDistributionGroup -Name "All GAV Resorts" -RecipientFilter {(RecipientType -eq 'UserMailbox') -and (userPrincipalName -like '*gavresorts.com.br')}
-
-# Obtendo informações do usuário do Exchange para usuários do SelectClub.com.br
-Get-MailGet-User -Filter "userPrincipalName -like '*selectclub.com.br'"
